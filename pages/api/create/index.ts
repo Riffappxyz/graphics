dbd73588bf7eedfb0b450696acacc306001f1301
Sweets Sweetman <sweetmantech@gmail.com>
2023-06-22 18:25:57 -0300

    small shipment to debug.

--- pages/api/create/index.ts
+++ pages/api/create/index.ts
@@ -20,15 +20,15 @@ export default async function handler(req, res) {
   console.log("imageBase64", imageBase64)
 
   // Call the /api/create-gif endpoint to create the GIF and upload it to nft.storage
-  const response2 = await fetch(`${appUrl}/api/create-gif`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ imageBase64, text: 'Top Collector' }),
-  }) as any;
-  console.log("imageBase64", imageBase64)
+  // const response2 = await fetch(`${appUrl}/api/create-gif`, {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ imageBase64, text: 'Top Collector' }),
+  // }) as any;
+  // console.log("imageBase64", imageBase64)
   
-  const { cid } = await response2.json();
-  console.log('Handler function finished', cid);
+  // const { cid } = await response2.json();
+  // console.log('Handler function finished', cid);
 
-  res.status(200).json({ cid });
+  res.status(200).json({ imageBase64 });
 }
