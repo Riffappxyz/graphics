b127c1c2ce71b2e58cb8fad664ebcd498b917cec
Sweets Sweetman <sweetmantech@gmail.com>
2023-06-23 11:15:34 -0300

    changed to 200 status in catch.

--- pages/api/create/index.ts
+++ pages/api/create/index.ts
@@ -28,6 +28,6 @@ export default async function handler(req, res) {
     res.status(200).json({ cid });
   } catch (error) {
     console.error('Failed to create gif', error);
-    return res.status(500).json({ message: 'Failed to create gif', error });
+    return res.status(200).json({ message: 'Failed to create gif', error });
   }
 }
