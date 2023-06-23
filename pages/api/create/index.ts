c9e2f348a045d2e7a1a8bbd958565ccb62a661cf
Sweets Sweetman <sweetmantech@gmail.com>
2023-06-23 11:03:36 -0300

    I remove unused var.

--- pages/api/create/index.ts
+++ pages/api/create/index.ts
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
   // console.log("imageBase64", imageBase64)
   
   // const { cid } = await response2.json();
-  console.log('Handler function finished', cid);
+  // console.log('Handler function finished', cid);
 
   res.status(200).json({ imageBase64, response2 });
 }
